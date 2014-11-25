0e433dba4725957c3907176422a4ae9af20dc832
fisherjrs <fisher.j.r.s@gmail.com>
2014-11-24 20:53:46 -0600

    adding sample enums

---
+++ src/enums/Planet.java
@@ -0,0 +1,36 @@
+package enums;
+
+public enum Planet {
+	Mercury(3.302e+23, 2.439e6),
+	Venus(4.869e+24, 6.052e6),
+	Earth(5.957e+24, 6.378e6);
+	
+	private final double mass;
+	private final double radius;
+	private final double surfaceGravity;
+	
+	private static final double G=6.67300E-11;
+	
+	Planet(double mass, double radius) {
+		this.mass = mass;
+		this.radius = radius;
+		surfaceGravity = G * mass / (radius * radius);
+	}
+	
+	public double mass() {
+		return mass;
+	}
+	
+	public double radius() {
+		return radius;
+	}
+	
+	public double surfaceGravity() {
+		return surfaceGravity;
+	}
+	
+	public double surfaceWeight(double mass) {
+		return mass * surfaceGravity;
+	}
+}
+
---
+++ src/enums/Red.java
@@ -0,0 +1,74 @@
+package enums;
+
+/*
+ * An enum type is used to hold a fixed set of constants. Use them instead of int enum patterns which were used before enum types were introduced in 1.5. 
+ * 
+ * Characteristics and benefits:
+ * 		Enum types are are effectively final because they have no accessible constructors. Clients can neither create instances of enum type nor extend them. They are generalized 
+ * 		singletons.
+ * 		Enums provide compile time type safety 
+ */
+public class Red {
+
+	public enum Apple { FUJI, BRAEBURN, PIPPIN };
+	public enum Regions { FUJI, BRAEBURN, PIPPIN };
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {		
+		
+		/** Sample of Planet enum in common **/
+		double earthWeight = Double.parseDouble((args.length == 0) ? "67" : args[0]);
+		double mass = earthWeight / Planet.Earth.surfaceGravity();
+		for (Planet p : Planet.values()) {
+			System.out.printf("Weight on %s is %f%n", p, p.surfaceWeight(mass));
+			System.out.printf("You are %s of the %s%n", "king", "world");
+		}
+		
+		/** Sample of Operation enum in common... it has an abstract method **/
+		System.out.printf("The result is %f, so stuff it.%n", Operation.PLUS.apply(23, 45));
+		System.out.printf("The result is %f, so stuff it.%n", Operation.MINUS.apply(23, 45));
+		System.out.printf("The result is %f, so stuff it.%n", Operation.TIMES.apply(23, 45));
+		System.out.printf("The result is %f, so stuff it.%n", Operation.DIVIDE.apply(23, 45));
+		
+				
+		Apple zz = Apple.FUJI;
+		Apple bb = Apple.FUJI;
+		
+		if( zz == bb ) {
+			System.out.println("Fuji is Braeburn ...??? ");
+		}else{
+			System.out.println("Fuji is not Braeburn!!!");
+		}
+		
+		//Compare ordinal position
+		System.out.println(zz.compareTo(bb));
+		System.out.println(bb.compareTo(zz));
+						
+		
+		/** Enums have their own namespace **/
+		Apple yy = Apple.FUJI;
+		Regions cc = Regions.FUJI;
+		
+		if(yy.equals(cc)) {
+			System.out.println("Apple.Fuji is Region.Fuji ...??? ");
+		}else{
+			System.out.println("Fuji apple and Fuji region are not the same!!!");
+		}
+		
+		if(cc.equals(Regions.FUJI)) {
+			System.out.println("The enum instance is equal to the enum definition.");
+		} else {
+			System.out.println("No the instance is not equal to the definition");
+		}
+		
+		System.out.println("hi :: " + cc.compareTo(Regions.FUJI));
+		if(cc.compareTo(Regions.BRAEBURN) == 1) {
+			System.out.println("The enum instance compares to the enum definition.");
+		} else {
+			System.out.println("No the instance doesn't compare to the definition");
+		}
+		
+	}
+}
