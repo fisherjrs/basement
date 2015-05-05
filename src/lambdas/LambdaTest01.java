495ad5757c7298f99f43962e968608484543a4e7
fisherjrs <fisher.j.r.s@gmail.com>
2015-05-05 06:16:53 -0500

    lambdas

---
+++ src/lambdas/LambdaTest01.java
@@ -0,0 +1,20 @@
+package lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class LambdaTest01 {
+	
+	public static Logger LOG = LogManager.getLogger(LambdaTest01.class);
+	
+	public static void main(String[] args) {
+		LOG.warn("Start Lamba Test 01");
+		List<String> names = Arrays.asList("1", "2", "3");
+		Stream<Object> lengths = names.stream().map(name -> name.length());
+	}
+}
