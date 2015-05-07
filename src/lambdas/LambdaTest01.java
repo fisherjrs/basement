f8ad457b646ed01979fb029ff74252a7cc216c24
fisherjrs <fisher.j.r.s@gmail.com>
2015-05-07 06:50:21 -0500

    debug output changes

--- src/lambdas/LambdaTest01.java
+++ src/lambdas/LambdaTest01.java
@@ -15,6 +15,8 @@ public class LambdaTest01 {
 	public static void main(String[] args) {
 		LOG.warn("Start Lamba Test 01");
 		List<String> names = Arrays.asList("1", "2", "3");
+		LOG.warn(names.get(0));
 		Stream<Object> lengths = names.stream().map(name -> name.length());
+		LOG.warn(lengths.count());
 	}
 }
