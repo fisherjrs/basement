a99be2200a3b5fe6640a63650ff35c55157b9156
fisherjrs <fisher.j.r.s@gmail.com>
2015-06-04 07:27:03 -0500

    lambdas

--- src/lambdas/WorkingWithStreams.java
+++ src/lambdas/WorkingWithStreams.java
@@ -15,11 +15,28 @@ public class WorkingWithStreams {
 	public static void main(String[] args) {
 		List<Dish> menu = Dish.menu;
 		List<Integer> someNumbers = Arrays.asList(1,2,3,4,5);
-		Optional<Integer> g = someNumbers.stream()
-				.map(x -> x * x)
-				.filter(x -> x % 3 == 0)
-				.findFirst();
-		LOG.debug("First integer squared divisible by 3:: {}", g.get());
+		int sum = someNumbers.stream().reduce(0, (a,b) -> a + b);
+		LOG.debug("sum {}", sum);
+		
+		int sum2 = someNumbers.stream().reduce(0, Integer::sum);
+		LOG.debug("sum2 {}", sum2);
+		
+		int max = someNumbers.stream().reduce(0, (a,b) -> Integer.max(a,b));
+		LOG.debug("max {}", max);
+		
+		
+		//Find the longest string
+		List<String> lines = Arrays.asList(
+				"John Lennon wore really cool sunglasses.",
+				"Tom Brady wins championships.",
+				"Archie Starbunkle has diamonds on his heels."
+				);
+		
+		Optional<Integer> listLength = lines.stream()
+				.map(l -> l.length())
+				.reduce((a,b) -> Integer.max(a,b));
+		
+		LOG.debug("length of string {}", listLength.get());
 	}
 	
 	public WorkingWithStreams() {
